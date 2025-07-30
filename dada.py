033d20b8b81a92053b33a53eea7650b877a43146
kaogitkub <kaogitkub@gmail.com>
2025-07-30 23:58:46 +0700

    dada.py

--- dada.py
+++ dada.py
@@ -5,3 +5,5 @@ print("Welcome to the program!")
 print("This is a simple Python script")
 print("Let's get started with some basic operations.")
 b = int(input("Enter a number: "))    
+print("You entered:", b)
+c = b * 2
